4d3b4f27eaa4f4c5a349ab1428f5af1e314fc34f
Anton Lofgren <anton.lofgren@gmail.com>
2013-12-29 22:51:33 +0100

    Add a test for lexing real numbers

--- test/gml_lexer_test.erl
+++ test/gml_lexer_test.erl
@@ -1,10 +1,19 @@
 -module(gml_lexer_test).
 -include_lib("eunit/include/eunit.hrl").
 
+tokens(String) ->
+	{ok, Tokens, _EndLine} = gml_lexer:string(String),
+	Tokens.
+
+real_number_test() ->
+	Tokens = tokens("realsies 4.1E9"),
+	?assertMatch([
+				  {key, _, "realsies", _},
+				  {real, _, 4.1e9}
+				 ], Tokens).
+
 simple_test() ->
-	Document = "graph [ somekey 1 ]",
-	{ok, Tokens, _EndLine} = gml_lexer:string(Document),
-	io:format("~p\n", [Tokens]),
+	Tokens = tokens("graph [ somekey 1 ]"),
 	?assertMatch([
 				  {key, _, "graph", 5},
 				  {'[', _},
